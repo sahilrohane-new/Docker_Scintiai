e32b476945e3fa099c42b852b559447a7798bc94
sahilrohane-new <sahil.rohane@ltimindtree.com>
2025-10-06 10:54:11 +0530

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://backend-service-567693257113.europe-west4.run.app", // Adjust if your FastAPI is at a different URL
+  baseURL: "https://backend-service-578618723567.europe-west1.run.app", // Adjust if your FastAPI is at a different URL
 });
 
 // Attach token to headers if present
